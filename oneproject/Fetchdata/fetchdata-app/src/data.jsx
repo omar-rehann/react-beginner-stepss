5e31e3a7fb71ccaef9d1108830e499cd0470b844
Omar Rehan <omarrehan724@gmail.com>
2025-10-20 20:26:26 +0300

    add search in fetch data

--- oneproject/Fetchdata/fetchdata-app/src/data.jsx
+++ oneproject/Fetchdata/fetchdata-app/src/data.jsx
@@ -1,23 +1,30 @@
-import { use, useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 import Structure from "./structure"
 import axios from "axios";
 function Data(){
     const [data,setdata]=useState([]);
-    const[eror,seteroro]=useState('')
+    const[eror,seteroro]=useState('');
+    const[value,setvalue]=useState('')
      useEffect(() => {
     axios
       .get("https://jsonplaceholder.typicode.com/posts")
       .then((response) => {
         setdata(response.data); 
       })
-      .catch((err) => {
+      .catch((eror) => {
         seteroro(eror.message);
       });
   }, []);
+ const post = data.filter((e) =>
+  e.title.toLowerCase().includes(value.toLowerCase())
+);
+
     return(
         <>
+        <div className="title">Fetch Data in table</div>
+        <input type="text" placeholder="Search In Table" onChange={(e)=>setvalue(e.target.value)} name="" id="" />
         {
-            data.map((e,i) =>{
+            post.map((e,i) =>{
              return(   <Structure key={i} Id={e.id} userId={e.userId} Title={e.title} Body={e.body}  />)
             })
         }
